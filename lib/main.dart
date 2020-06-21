d087ad78faf91e981577ff18f5ca5a11a86e588d
anasvakyathodi <anasvakyathodi@gmail.com>
2020-06-21 12:21:52 +0530

    appbar color change using themeData()

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: HomePage(),
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+      ),
+    );
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New App'),
+      ),
+      body: Container(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
